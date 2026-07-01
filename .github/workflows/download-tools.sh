f1a68f52561a67952aa24f36fc868fc36205c1f0
Sourabh Mehta <73165318+soumeh01@users.noreply.github.com>
2026-07-01 11:29:35 +0200

    Drop macOS amd64 build and release binaries (#350)

--- .github/workflows/download-tools.sh
+++ .github/workflows/download-tools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT=$(basename "$0")
-PLATFORMS=(win32-x64 win32-arm64 linux-x64 linux-arm64 darwin-x64 darwin-arm64)
+PLATFORMS=(win32-x64 win32-arm64 linux-x64 linux-arm64 darwin-arm64)
 
 set -eo pipefail
 
